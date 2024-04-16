613291b34194568c622352d4fe600df773742cca
tuddy <tuddy18_26@yahoo.com>
2024-04-16 18:50:58 +0300

    Integrated GTest framework

---
+++ Google_tests/test_empty.cpp
@@ -0,0 +1,18 @@
+//
+// Created by tuddy on 16-Apr-24.
+//
+
+#include <gtest/gtest.h>
+
+TEST(TestEmpty, AssertIntEQ) {
+
+    EXPECT_EQ(4, 4);
+
+}
+
+
+TEST(TestEmpty, AssertDoubleEQ) {
+
+    EXPECT_EQ(3.14, 3.14);
+
+}
